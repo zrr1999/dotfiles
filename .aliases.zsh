87875d1f445bcbb0baf1473af0b6fc55cddd5dc6
Zhan Rongrui <2742392377@qq.com>
2023-07-26 08:22:11 +0000

    init

---
+++ .aliases.zsh
@@ -0,0 +1,21 @@
+
+# add some alias
+alias j='z'
+alias grep='rg'
+alias ls='lsd'
+alias l='ls -l'
+alias la='ls -a'
+alias lla='ls -la'
+alias wget='http --download'
+tree () {
+    local depth=3
+    (($+1)) && {
+        local depth=$1
+    }
+    ls --tree --depth $depth
+}
+
+alias -s gz='tar -xzvf'
+alias -s tgz='tar -xzvf'
+alias -s zip='unzip'
+alias -s bz2='tar -xjvf'
