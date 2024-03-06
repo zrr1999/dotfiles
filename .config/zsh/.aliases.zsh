9c48cbc556a4d9840064cdf0bcef0b14c3b22a63
zrr1999 <2742392377@qq.com>
2024-03-06 12:41:42 +0800

    🐛 fix: add pip alias

--- .config/zsh/.aliases.zsh
+++ .config/zsh/.aliases.zsh
@@ -2,6 +2,7 @@
 alias sudo='sudo -E '
 alias sizeof="du -h --max-depth=0"
 alias python='python3'
+alias pip='pip3'
 alias urldecode='python3 -c "import sys, urllib.parse as ul; print(ul.unquote_plus(sys.argv[1]))"'
 alias urlencode='python3 -c "import sys, urllib.parse as ul; print(ul.quote_plus(sys.argv[1]))"'
 alias pd='pueued -d'
